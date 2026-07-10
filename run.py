750bb9a1da87645a071d7967ec21bbdb67556f84
emmanuelkorir <emmanuelqkorir@gmail.com>
2026-07-10 15:53:11 +0300

    add configuration for run.py and requirements.txt

--- run.py
+++ run.py
@@ -9,12 +9,13 @@ import asyncio
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
+import os
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=int(os.getenv("PORT", 8000)),
         reload=False,
     )
