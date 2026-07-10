81aaf44af1893a610b96b64702c236ee7bcb8cac
emmanuelkorir <emmanuelqkorir@gmail.com>
2026-07-10 17:44:50 +0300

    edit main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=int(os.getenv("PORT", 8000)),
         reload=False,
     )
